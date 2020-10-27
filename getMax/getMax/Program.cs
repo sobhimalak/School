b8b257b786d73799c2384bd81594e112afb97b52
Sobhi Malak <sobhi@cloudulu.com>
2020-10-27 15:23:27 +0100

    modified

---
+++ getMax/getMax/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace getMax
+{
+    class Program
+    {
+          static int number;
+
+            static void GetMax(int first, int second)
+            {
+                if (first > second) number = first;
+                else number = second;
+            }
+
+            static void Main(string[] args)
+            {
+                Console.Write("Enter first number: ");
+                number = int.Parse(Console.ReadLine());
+                Console.Write("Enter second number: ");
+                int number2 = int.Parse(Console.ReadLine());
+
+                GetMax(number, number2);
+            {
+                Console.WriteLine("The largets number is {0}", number);
+            }
+            }
+    }
+}
