b6f6caf301e4059ca45693655525b76f7437b43e
Sobhi Malak <sobhi@cloudulu.com>
2020-10-27 10:11:28 +0100

    practice

---
+++ Helloworld/Helloworld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PracticeCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello World!");            // hello world practice
+
+            Console.WriteLine("Hello");                            //hello +  Name practice
+            Console.WriteLine("Sobhi");
+
+            Console.Write("What is favorite color?");           // What is your avorite color? practice
+            Console.Read();
+            Console.WriteLine("Mine too!");
+
+
+             Console.Write("What is your Name? " );          // What is your name? practice
+             string name= Console.ReadLine();
+             Console.WriteLine("Nice to meet you " + name);
+            */
+            //TODO
+        }
+    }
+}
