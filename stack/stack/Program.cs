1b37e0679c3f3f8086c7aeef8fd82d7c82a69a09
Sobhi Malak <sobhi@cloudulu.com>
2020-10-28 16:24:15 +0100

    more exersices

---
+++ stack/stack/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace stackrevese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var text = "Learning Csharp";
+            
+            Console.WriteLine("Regular :  " + text);
+            
+
+            var stack = new Stack ();
+
+            foreach (char c in text)
+                stack.Push(c);
+
+            text = string.Empty;
+          
+
+            while (stack.Count > 0)
+                text += stack.Pop();
+
+            Console.WriteLine("Backwards :  " + text);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ streamreader/streamreader/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace File_Example
+{
+    class Program
+    {
+        public void WriteOperation()
+        {
+            FileStream fs = new FileStream("d:\\test.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter w = new StreamWriter(fs);
+            w.WriteLine(Console.ReadLine());
+            w.Close();
+            fs.Close();
+        }
+        public void ReadOperation()
+        {
+            FileStream fs = new FileStream("d:\\test.txt", FileMode.Open, FileAccess.Read);
+            StreamReader r = new StreamReader(fs);
+            int counter = 0;
+            while (!r.EndOfStream)
+            {
+                Console.WriteLine(r.ReadLine());
+                counter++;
+            }
+            Console.WriteLine(counter);
+            r.Close();
+            fs.Close();
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.WriteOperation();
+            p.ReadOperation();
+        }
+    }
+}
